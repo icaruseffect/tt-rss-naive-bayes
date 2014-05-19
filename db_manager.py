345c3bdd801ae26c314b9781b4b272387b74ee27
Icaruseffect <you@example.com>
2014-05-19 12:50:17 +0200

    modularisation of read/write to the database part 1

--- db_manager.py
+++ db_manager.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import MySQLdb
+import MySQLdb.cursors
 from helpers import *
 
 class Database_Manager(object):
@@ -8,37 +9,40 @@ class Database_Manager(object):
 
 	def __init__(self, thread_id=0):
 		self.thread_id=thread_id
-		debug_mode( "mysql_manager started" ,1 )
+		debug_mode( "mysql_manager started" ,0 )
 		self.var_db_prefix=config.get('server','database_prefix')
 
 		self.connected=False
 		self.connect_database()
+		self.disconnect_database()
 
-	def stop(self):
+	def disconnect_database(self):
 		self.ttrss_db.close()
+		debug_mode("connection closed",3)
 
 
 	def connect_database(self):
 		'connects to the mysql database, based on the configuration file with detailed connection to a local/remote host or on a local server with data from my.conf'
+		common_parameters= " use_unicode = 'True'  my_conv= {id : int, score : int, content: unicode, title : unicode} "
 
-		debug_mode( "connecting to database" ,2 )
 		if config.get('server','use_my_cfg') == True:
-
+			debug_mode( "connecting to database with my.cnf" ,3 )
 			self.ttrss_db = MySQLdb.connect(host=config.get('server', 'host'),
 											db=config.get('server','database'),
 											read_default_file="~/.my.cnf"
 											)
 
 		else:
+			debug_mode( "connecting to database with login data from config" ,3 )
 			self.ttrss_db = MySQLdb.connect(host=config.get('server', 'host'),
 											user=config.get('server','user'),
 											passwd=config.get('server','password'),
-											db=config.get('server','database'),
-											port=int (config.get('server','port') )
+											db=config.get('server','database')##,
+											#port=int (config.get('server','port') )
 											)
-		debug_mode( "connection established" ,2)
+		debug_mode( "connection established" ,3)
+		#self.db_cursor = self.ttrss_db.cursor()
 		self.db_cursor = self.ttrss_db.cursor()
-
 
 		self.connected=True
 		return self.connected
@@ -74,8 +78,7 @@ class Database_Manager(object):
 			labels = {'label': None}
 		return labels
 
-	def write_score(self,entry_id, score=0):
-		query_entry = ( "UPDATE "+ str(config.get('server','database') ) + "." + self.var_db_prefix + "user_entries SET score = " + str(score) +" WHERE ref_id =  " + str(entry_id) )
+
 		return
 
 #### rewrite of above functions ###
@@ -84,25 +87,31 @@ class Database_Manager(object):
     ##so there is only the need to handle the read/write head (in future possible a sub_class, depending on benchmarks)
     ##quick and dirty tuturial: http://ianhowson.com/a-quick-guide-to-using-mysql-in-python.html
 	def write_to_table(self, selector_list, content_list):
-		'this function handles common writing procedures to the database'
-		return
+		'this function handles common reading, writing and updating of tables including the database head'
 
-	def update_table(self, selector_list):
-		'handles updating of articles and bag of words'
-		return
-
-	def read_table(self, selector_list):
-		'handles reading of tables and bag of words from the database'
-		return
+		#1) execute (select db rows )
+		#2) execute ( select content to read/write)
+		#3) if read command -> cursor.fetchall()
+		return fetchoneDict(c)
 
 	def translate_request(self,request, article_id):
 		'translates requests from article manager to mysql selectors based on request type (write,read,update) '
 		request_translations={} #update, read,write
 		self.queries = {
+			"score" :( "UPDATE " + self.var_db_prefix + "user_entries SET score = " + str(score) +" WHERE ref_id =  " + str(entry_id) ),
 			"starred" : ("SELECT ref_id FROM  "+self.var_db_prefix+"user_entries WHERE (marked = 1) OR (published = 1)"),
 			"read" : ("SELECT ref_id FROM " +self.var_db_prefix+"user_entries WHERE (marked = 0) AND (published = 0) AND (unread = 0) "),
 			"unread" : ("SELECT ref_id FROM "+self.var_db_prefix+"user_entries WHERE (score = '0')"),
+			"content" : ("SELECT id, title, content FROM " +self.var_db_prefix+"entries WHERE (id="+ str(entry_id) +")"),
 			}
+		translations= { 'read' : 'SELECT',
+						'write' : 'UPDATE',
+						'standard1' : ('ref_id FROM ' + self.var_db_prefix + 'user entries WHERE' ),
+						'starred' : ('(marked = 1) OR (published = 1)' ),
+						'read' : ('(marked = 0) AND (published = 0) AND (unread = 0)' ),
+						'unread' : (' score = 0' ),
+						'score' : ('SET score = ' + int(content) )
+					}
 		return
 
 #### usage reference:
